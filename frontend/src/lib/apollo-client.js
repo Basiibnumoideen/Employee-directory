f6a70023fe0f0fd058da0d3f13f08b914209d8fa
BASI <98819361+Basiibnumoideen@users.noreply.github.com>
2025-06-27 15:57:28 +0530

    Update apollo-client.js

--- frontend/src/lib/apollo-client.js
+++ frontend/src/lib/apollo-client.js
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri:  'https://employee-directory-9fnq.onrender.com',
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT, // ✅ From .env
   cache: new InMemoryCache(),
 });
 
